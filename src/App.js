4d8175a4d38133c27e3566c652949726b4537500
egoing <egoing@gmail.com>
2023-11-13 13:31:12 +0900

    이벤트 처리 : 리액트에서 이벤트의 이름은 onclick X -> onClick O 이벤트 props는 문자열 X ->함수 O
    상태 처리 :
    useState로 state를 만든다.
    useState의 첫번째 파라미터는 초기값이다.
    state는 두개의 원소로 이루어진 배열이다.
    첫번째 원소는 값을 읽을 때 쓴다.
    두번째 원소는 값을 변경할 때 쓰는 함수다.
    두번째 원소에 값을 입력했을 때 값이 달라지면 컴포넌트 함수가 다시 실행된다.
    달라진 상태의 값에 따라서 다른 UI를 출력한다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 
 function Counter({ title, initValue }) {
+  const countState = useState(initValue);
+  const count = countState[0];
+  const setCount = countState[1];
+  function clickHandler() {
+    setCount(count + 1);
+  }
   return (
     <>
       <h1>{title}</h1>
-      <button>+</button> {initValue}
+      <button onClick={clickHandler}>+</button> {count}
     </>
   );
 }
