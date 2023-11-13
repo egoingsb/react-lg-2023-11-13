05649c8489c58fb5de0c358132e20c76c0da2787
egoing <egoing@gmail.com>
2023-11-13 10:34:56 +0900

    기본 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div>안녕하세요!</div>;
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>+</button> 0
+    </div>
+  );
 }
 
 export default App;
